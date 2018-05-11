349f840212ad50762bd41bb0770d596f0322da56
Kenji Lozano <kenji.lozano.dev@gmail.com>
2018-05-11 16:19:31 -0400

    Adds react router dependency and setup basic routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Home from './home/Home.js';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+import { BrowserRouter } from 'react-router-dom';
+import { Route } from 'react-router';
+
+const App = () => (
+  <BrowserRouter>
+    <div>
+      {/* Defined routes */}
+      <Route exact path="/" component={Home}/>
+      <Route path="/home" component={Home}/>
+    </div>
+  </BrowserRouter>
+)
 
 export default App;
