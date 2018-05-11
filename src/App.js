dce758318f9de91a870dcca4bb949b4a8f2fe4a5
Kenji Lozano <kenji.lozano.dev@gmail.com>
2018-05-11 16:38:28 -0400

    Added placeholder for users list

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Home from './home/Home.js';
+import Users from './users/Users';
 
 import { BrowserRouter } from 'react-router-dom';
 import { Route } from 'react-router';
@@ -9,8 +9,8 @@ const App = () => (
   <BrowserRouter>
     <div>
       {/* Defined routes */}
-      <Route exact path="/" component={Home}/>
-      <Route path="/home" component={Home}/>
+      <Route exact path="/" component={Users}/>
+      <Route path="/users" component={Users}/>
     </div>
   </BrowserRouter>
 )
