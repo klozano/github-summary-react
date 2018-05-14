40ad357a2983d848cf77fe78c523c04d0a5e56b3
Kenji Lozano <kenji.lozano.dev@gmail.com>
2018-05-13 22:44:12 -0400

    Enabled pagination over repositories list

--- src/repositories/Repositories.js
+++ src/repositories/Repositories.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import queryString from 'query-string';
-import { Grid } from 'semantic-ui-react';
+import { Grid, Button, Icon, Segment } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
 import Repository from './Repository';
 
-const REPO_PAGE_SIZE = 4;
+const REPO_PAGE_SIZE = 8;
 class Repositories extends React.Component {
   constructor() {
     super();
     this.state = {
       loading: true,
       repositories: [],
+      page: 1,
     };
   }
 
@@ -19,37 +21,62 @@ class Repositories extends React.Component {
     this.__getRepositories();
   }
 
-  __getRepositories() {
-    const { match, location } = this.props;
-    const { page = 0 } = queryString.parse(location.search);
+  componentWillReceiveProps(newProps) {
+    this.__getRepositories(newProps);
+  }
+
+  __getRepositories(props) {
+    const { match, location } = props || this.props;
+    const { page = 1 } = queryString.parse(location.search);
     fetch(`https://api.github.com/users/${match.params.username}/repos?page=${page}&per_page=${REPO_PAGE_SIZE}`)
       .then(response => response.json())
       .then(
         repositories => {
-          this.setState({ repositories, loading: false });
+          this.setState({ repositories, loading: false, page });
         }
       )
   }
 
   render() {
-    const { repositories } = this.state;
+    const { repositories, page } = this.state;
+    const { match } = this.props;
     return (
-      <Grid container columns={4}>
-        {
-          repositories.map(({ id, name, full_name, description, html_url, forks, open_issues_count, open_issues }) => (
-            <Grid.Column key={id}>
-              <Repository
-                name={name}
-                fullname={full_name}
-                description={description}
-                url={html_url}
-                issues={open_issues}
-                openedIssues={open_issues_count}
-                forks={forks}/>
-            </Grid.Column>
-          ))
-        }
-      </Grid>
+      <React.Fragment>
+        <Grid container stretched columns={4}>
+          {
+            repositories.map(({ id, name, full_name, description, html_url, forks, open_issues_count, open_issues }) => (
+              <Grid.Column key={id}>
+                <Repository
+                  name={name}
+                  fullname={full_name}
+                  description={description}
+                  url={html_url}
+                  issues={open_issues}
+                  openedIssues={open_issues_count}
+                  forks={forks}/>
+              </Grid.Column>
+            ))
+          }
+        </Grid>
+        <Segment basic textAlign='center'>
+          {
+            (+page > 1) && <Link to={`/repositories/${match.params.username}?page=${+page - 1}`}>
+              <Button icon labelPosition='left' disabled={ page === 1}>
+                <Icon name='left arrow' />
+                Prev. Page
+              </Button>
+            </Link>
+          }
+          {
+            (repositories.length !== 0) && <Link to={`/repositories/${match.params.username}?page=${+page + 1}`}>
+              <Button icon labelPosition='right'>
+                Next Page
+                <Icon name='right arrow' />
+              </Button>
+            </Link>
+          }
+        </Segment>
+      </React.Fragment>
     );
   }
 }
